0592d07b9d80506ba2157a563adbd0f0eadd71c3
Danmar Varela <vareladanmar@gmail.com>
2022-09-14 05:56:22 +0800

    Format files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import {ColorModeScript} from "@chakra-ui/react";
+import React, {StrictMode} from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
+import {BrowserRouter} from "react-router-dom";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 root.render(
