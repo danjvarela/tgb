8595196d94351cbac8493ce5478d2284f9b55c22
Danmar Varela <vareladanmar@gmail.com>
2022-09-14 19:20:12 +0800

    Customize breakpoints

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import {ChakraProvider, Box, HStack, theme, VStack} from "@chakra-ui/react";
+import {ChakraProvider, Box, HStack, extendTheme, VStack} from "@chakra-ui/react";
 import {ColorModeSwitcher} from "ColorModeSwitcher";
 // import CreateAdminPage from "scenes/CreateAdminPage";
 import LoginAdminPage from "scenes/LoginAdminPage";
+
+const theme = extendTheme({
+  breakpoints: {
+    sm: "320px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+    "2xl": "1536px",
+  },
+});
 
 function App() {
   return (
