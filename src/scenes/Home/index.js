59fd16e0dc3f9e53c71990b5648aa5c22455f3ca
Danmar Varela <vareladanmar@gmail.com>
2022-09-16 02:22:58 +0800

    Modify initial homepage

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,5 +1,15 @@
+import {Box, HStack} from "@chakra-ui/react";
+import {ColorModeSwitcher} from "ColorModeSwitcher";
+
 const Home = () => {
-  return <>This is home</>;
+  return (
+    <Box w="full" h="full">
+      <HStack justifyContent="flex-end" px={{base: 3, md: 9}} py={1} w="full">
+        <ColorModeSwitcher />
+      </HStack>
+      This is home
+    </Box>
+  );
 };
 
 export default Home;
