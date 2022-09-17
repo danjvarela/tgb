ab61842f81c3fda8680455f2a10f662fff36df18
Danmar Varela <vareladanmar@gmail.com>
2022-09-17 14:09:44 +0800

    Configure app so it can be deployed to gh-pages

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <HashRouter basename="/tgb">
+    <BrowserRouter basename="/tgb">
       <ColorModeScript />
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
 
